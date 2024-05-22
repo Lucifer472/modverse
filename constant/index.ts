c9da005606e9991765721cb40194117c0626227c
Lucifer472 <JanvI@0427>
2024-05-22 14:53:43 +0530

    mod verse

--- constant/index.ts
+++ constant/index.ts
@@ -1,31 +1,19 @@
 export const category = [
   {
-    label: "Government Yojana",
-    link: "/government-yojana",
+    label: "Apk",
+    link: "/apk",
   },
   {
-    label: "Application",
-    link: "/application",
+    label: "Technology",
+    link: "/technology",
   },
   {
-    label: "News",
-    link: "/news",
+    label: "Trending",
+    link: "/trending",
   },
   {
-    label: "Government Schemes",
-    link: "/government-schema",
-  },
-  {
-    label: "Scholarship",
-    link: "/scholarship",
-  },
-  {
-    label: "Jobs",
-    link: "/jobs",
-  },
-  {
-    label: "Exams",
-    link: "/exams",
+    label: "Gadgets",
+    link: "/gadgets",
   },
 ];
 
@@ -62,8 +50,8 @@ export const faq = {
     "This is the First Qustion that need to be Answered 2",
   ],
   answer: [
-    "This is the First Question and it answer that we suppose to be here it is the first question and the Answet remebered that okay 1",
-    "This is the First Question and it answer that we suppose to be here it is the first question and the Answet remebered that okay 2",
+    "This is the First Question and it answer that we suppose to be here it is the first question and the Answer remembered that okay 1",
+    "This is the First Question and it answer that we suppose to be here it is the first question and the Answer remembered that okay 2",
   ],
 };
 
@@ -72,9 +60,9 @@ export const byUrl = "https://images.drivingexamexpert.com/upload/url.php";
 export const byProfile =
   "https://images.drivingexamexpert.com/upload/profile.php";
 
-export const url = "https://missiongujarat.in";
-export const title = "Mission Gujarat";
-export const description = `Welcome to missiongujarat.in, your go-to resource for comprehensive Driving Exam Blogs in Australia. Whether you're preparing for your learner's permit or aiming to ace your practical driving test, we provide valuable insights, expert tips, and firsthand experiences tailored to the Australian driving landscape. Navigate the road to success with our in-depth guides on driving test routes, regional road rules, and city-specific exam strategies. Stay updated on the latest changes in Australian driving regulations and boost your confidence behind the wheel. Join our community of learners and experienced drivers as we share the knowledge that drives success on the Australian roads. Start your journey to driving excellence today with missiongujarat.in!`;
+export const url = "https://modzverse.com";
+export const title = "Modz Verse";
+export const description = `Welcome to modzverse.com, your go-to resource for comprehensive Driving Exam Blogs in Australia. Whether you're preparing for your learner's permit or aiming to ace your practical driving test, we provide valuable insights, expert tips, and firsthand experiences tailored to the Australian driving landscape. Navigate the road to success with our in-depth guides on driving test routes, regional road rules, and city-specific exam strategies. Stay updated on the latest changes in Australian driving regulations and boost your confidence behind the wheel. Join our community of learners and experienced drivers as we share the knowledge that drives success on the Australian roads. Start your journey to driving excellence today with missiongujarat.in!`;
 export const keywords =
   "Driving exam tips, Learner's permit Australia, Australian driving test, Practical driving guide, Road test strategies, Driving lessons advice, Regional road rules, Australia driving experience, Driving test preparation, Aussie road safety, Driver education blog, Defensive driving skills, Australian driving laws, Driver's license journey, Traffic rules in Australia, Behind the wheel insights, Road test success stories, Driving confidence tips, Driving school recommendations, Australian road signs";
 export const CardImage = "/images";
